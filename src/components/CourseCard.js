95179f4ed5a8ceeacffd558dc3f3f4abc946b5d2
sinmim-0816 <janefam88@gmail.com>
2026-04-05 13:36:07 +0800

    update Course and CourseCard

---
+++ src/components/CourseCard.js
@@ -0,0 +1,74 @@
+import { View, Pressable, Image, Text, StyleSheet} from 'react-native';
+import {BookOpenText, CheckCheck, Timer} from 'lucide-react-native'
+
+const CourseCard=({imagePath, courseTitle, numModules,duration})=>{
+    return (
+        <View style={styles.card}>
+            <Image source={imagePath} style={styles.courseImg} accessibilityLabel='Cover Photo of Course'/>
+            <Text style={styles.CourseTitle}>{courseTitle}</Text>
+            
+            <View style={styles.courseDetails}>
+                <BookOpenText size={20}/>
+                <Text style={styles.DetailsText}>{numModules} Modules</Text>
+            </View>
+            <View style={styles.courseDetails}>
+                <Timer size={20}/>
+                <Text style={styles.DetailsText}>{duration}</Text>
+            </View>
+            <Pressable style={styles.Publishbtn}>
+                <CheckCheck/>
+                <Text>Published</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    card:{
+        flex:1,
+        backgroundColor:"white",
+        borderRadius:5,
+        paddingHorizontal:15,
+        paddingVertical:10,
+        borderRadius:10
+    },
+    courseImg:{
+        width:250,
+        height:180,
+        resizeMode:'contain',
+        alignSelf:'center'
+    },
+    CourseTitle:{
+        borderBottomColor:'#8f8f8f',
+        borderBottomWidth:1,
+        fontSize:19,
+        paddingVertical:10,
+        marginBottom:10,
+        textAlign:'center',
+        fontWeight:'bold'
+    },
+    courseDetails:{
+        flexDirection:'row',
+        alignContent:'center',
+        padding:2,
+        color:'#3e3e3e',
+        gap:5
+    },
+    DetailsText:{
+        color:'#3e3e3e',
+        fontSize:14
+    },
+    Publishbtn:{
+        flexDirection:'row',
+        gap:8,
+        alignItems:'center',
+        alignSelf:'center',
+        backgroundColor:'#ffc146',
+        borderRadius:10,
+        paddingHorizontal:20,
+        paddingVertical:8,
+        marginTop:15,
+    },
+});
+
+export default CourseCard;
--- src/screens/AdminCourse.js
+++ src/screens/AdminCourse.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet, Pressable, TextInput, Image} from 'react-native';
+import { View, Text, StyleSheet, Pressable, TextInput, Image, ImageBackground} from 'react-native';
 import {CopyPlus, Search, BookOpenText, Timer, CheckCheck} from 'lucide-react-native';
+
+import CourseCard from '../components/CourseCard.js';
 
 
 const AdminCourse = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.courseContainer}>
-                <View>
-                    <Text style={styles.description}>Here you can find all courses</Text>
-                    <Text style={styles.title}>All Courses</Text>
+            <ImageBackground 
+                source={require('../../assets/forest.png')}
+                style={styles.backgroundImage}
+            >
+                <View style={styles.courseContainer}>
+                    <View>
+                        <Text style={styles.description}>Here you can find all courses</Text>
+                        <Text style={styles.title}>All Courses</Text>
+                    </View>
+                    <Pressable style={({ hovered }) => [
+                            styles.btn,
+                            hovered && styles.btnHover, 
+                        ]}>
+                        <CopyPlus/>
+                        <Text style={styles.btnText}>Add Course</Text>
+                    </Pressable>
                 </View>
-                <Pressable style={({ hovered }) => [
-                        styles.btn,
-                        hovered && styles.btnHover, 
-                    ]}>
-                    <CopyPlus/>
-                    <Text style={styles.btnText}>Add Course</Text>
-                </Pressable>
-            </View>
+            </ImageBackground>
             {/* Search */}
             <View style={styles.search}>
                 <Search size={18}/>
@@ -27,72 +34,10 @@ const AdminCourse = () => {
 
             {/* Course Card */}
             <View style={styles.cardContainer}>
-                <View style={styles.card}>
-                    <Image source={require('../../assets/first_aid.png')} style={styles.courseImg} accessibilityLabel='Cover Photo of Course'/>
-                    <Text style={styles.CourseTitle}>Basic First Aid</Text>
-                    
-                    <View style={styles.courseDetails}>
-                        <BookOpenText size={20}/>
-                        <Text style={styles.DetailsText}>15 Modules</Text>
-                    </View>
-                    <View style={styles.courseDetails}>
-                        <Timer size={20}/>
-                        <Text style={styles.DetailsText}>15 hours 30 mins</Text>
-                    </View>
-                    <Pressable style={styles.Publishbtn}>
-                        <CheckCheck/>
-                        <Text>Published</Text>
-                    </Pressable>
-                </View>
-                <View style={styles.card}>
-                    <Image source={require('../../assets/first_aid.png')} style={styles.courseImg} accessibilityLabel='Cover Photo of Course'/>
-                    <Text style={styles.CourseTitle}>Basic First Aid</Text>
-                    <View style={styles.courseDetails}>
-                        <BookOpenText size={20}/>
-                        <Text style={styles.DetailsText}>15 Modules</Text>
-                    </View>
-                    <View style={styles.courseDetails}>
-                        <Timer/>
-                        <Text>15 hours 30 mins</Text>
-                    </View>
-                    <Pressable style={styles.btn}>
-                        <CopyPlus/>
-                        <Text>Published</Text>
-                    </Pressable>
-                </View>
-                <View style={styles.card}>
-                    <Image source={require('../../assets/first_aid.png')} style={styles.courseImg} accessibilityLabel='Cover Photo of Course'/>
-                    <Text style={styles.CourseTitle}>Basic First Aid</Text>
-                    
-                    <View style={styles.courseDetails}>
-                        <BookOpenText size={20}/>
-                        <Text style={styles.DetailsText}>15 Modules</Text>
-                    </View>
-                    <View style={styles.courseDetails}>
-                        <Timer/>
-                        <Text>15 hours 30 mins</Text>
-                    </View>
-                    <Pressable style={styles.btn}>
-                        <CopyPlus/>
-                        <Text style={styles.btnText}>Published</Text>
-                    </Pressable>
-                </View>
-                <View style={styles.card}>
-                    <Image source={require('../../assets/first_aid.png')} style={styles.courseImg} accessibilityLabel='Cover Photo of Course'/>
-                    <Text style={styles.CourseTitle}>Basic First Aid</Text>
-                    <View style={styles.courseDetails}>
-                        <BookOpenText size={20}/>
-                        <Text style={styles.DetailsText}>15 Modules</Text>
-                    </View>
-                    <View style={styles.courseDetails}>
-                        <Timer/>
-                        <Text>15 hours 30 mins</Text>
-                    </View>
-                    <Pressable style={styles.btn}>
-                        <CopyPlus/>
-                        <Text style={styles.btnText}>Published</Text>
-                    </Pressable>
-                </View>
+                <CourseCard imagePath={require('../../assets/first_aid.png')} courseTitle="Basic First Aid" numModules={15} duration="15 hours 30 mins"/>
+                <CourseCard imagePath={require('../../assets/first_aid.png')} courseTitle="Basic First Aid" numModules={15} duration="15 hours 30 mins"/>
+                <CourseCard imagePath={require('../../assets/first_aid.png')} courseTitle="Basic First Aid" numModules={15} duration="15 hours 30 mins"/>
+                <CourseCard imagePath={require('../../assets/first_aid.png')} courseTitle="Basic First Aid" numModules={15} duration="15 hours 30 mins"/>
             </View>
         </View>
     );
@@ -107,30 +52,38 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 10,
+        color:'white'
     },
     description: {
         fontSize: 14,
         lineHeight: 24,
-        color:'#8f8f8f'
+        color:'white',
     },
     courseContainer:{
-        backgroundColor:'#ffffff',
         paddingHorizontal:40,
         paddingVertical:30,
         borderRadius:20,
         flexDirection:'row',
-        justifyContent:'space-between'
+        justifyContent:'space-between',
+        
+    },
+    backgroundImage:{
+        width:'100%',
+        borderRadius:20,
+        overflow:'hidden',
+        resizeMode:'fill',
+        marginTop:10,
     },
     btn:{
         flexDirection:'row',
         gap:8,
         alignItems:'center',
         alignSelf:'center',
-        backgroundColor:'#0a6340',
+        backgroundColor:'#50984d',
         borderRadius:50,
         color:'white',
         paddingHorizontal:23,
-        paddingVertical:13
+        paddingVertical:13,
     },
     btnHover:{
         backgroundColor:'#4ba525fe'
@@ -156,58 +109,13 @@ const styles = StyleSheet.create({
         paddingVertical:2,
         outlineStyle:'none'
     },
-    courseImg:{
-        width:250,
-        height:180,
-        resizeMode:'contain',
-        alignSelf:'center'
-    },
-    card:{
-        flex:1,
-        backgroundColor:"white",
-        borderRadius:5,
-        paddingHorizontal:15,
-        paddingVertical:10,
-        borderRadius:10
-    },
     cardContainer:{
         flexDirection:'row',
         gap:30,
         marginHorizontal:20,
         marginTop:20,
         
-    },
-    CourseTitle:{
-        borderBottomColor:'#8f8f8f',
-        borderBottomWidth:1,
-        fontSize:19,
-        paddingVertical:10,
-        marginBottom:10,
-        textAlign:'center',
-        fontWeight:'bold'
-    },
-    courseDetails:{
-        flexDirection:'row',
-        alignContent:'center',
-        padding:2,
-        color:'#3e3e3e',
-        gap:5
-    },
-    DetailsText:{
-        color:'#3e3e3e',
-        fontSize:14
-    },
-    Publishbtn:{
-        flexDirection:'row',
-        gap:8,
-        alignItems:'center',
-        alignSelf:'center',
-        backgroundColor:'#ffc146',
-        borderRadius:50,
-        paddingHorizontal:23,
-        paddingVertical:10,
-        marginTop:15,
-    },
+    }
 });
 
 export default AdminCourse;
